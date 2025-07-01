ae0bd79544479b7c2737ca1a76f15a67237c3eec
BinaryOracle <zdhdhyzdhdhy@gmail.com>
2025-07-01 21:14:39 +0800

    chapter-2-02

---
+++ chapter2/__init__.py
@@ -0,0 +1,10 @@
+# dezero/__init__.py
+from chapter2.core import Variable
+from chapter2.core import Function
+from chapter2.core import using_config
+from chapter2.core import no_grad
+from chapter2.core import as_array
+from chapter2.core import as_variable
+from chapter2.core import setup_variable
+
+setup_variable()  # 初始化运算符重载
